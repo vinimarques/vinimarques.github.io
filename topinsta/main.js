7a9495cf74ad77c608b9ac861f5e512040de380c
Vinicius Roloff Marques <vinicius.roloffmarques@gmail.com>
2015-08-12 02:50:46 -0300

    test instagram api

--- topinsta/main.js
+++ topinsta/main.js
@@ -18,8 +18,9 @@
 
 
 		this.token = "1836111965.7b3c14d.b3f331b1e30d4c9c85916fb554136bcc";
+		this.clientId = "7b3c14d21dc04cdd971c2a262facfef3";
 
-		$.get('https://api.instagram.com/v1/users/self/media/liked/?access_token=' + this.token , function(data) {
+		$.get('https://api.instagram.com/v1/users/self/media/liked/?client_id=' +  this.clientId + '&access_token=' + this.token , function(data) {
 			console.log(data);
 		});
 
